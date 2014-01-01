ce18197c781bb5513c36be569478e7516766b202
Vadym Tyemirov <temirov@gmail.com>
2014-01-01 17:28:42 +0700

    Using on_change hook in a model added for triggering an event

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,32 +2,45 @@ require 'reloader/sse'
 
 class DashboardController < ApplicationController
   include ActionController::Live
+  respond_to :html, :js
   
   def show
     @time = Time.now
-    
+    respond_with @time
   end
   
-  def update
+  def events
     response.headers['Content-Type'] = 'text/event-stream'
-    
+
     sse = Reloader::SSE.new(response.stream)
-    
-    # 100.times {
-    #   response.stream.write "hello world\n"
-    # }
-    # response.stream.close
-    
-    begin
-      loop do
-        sse.write({ time: Time.now }, event: 'refresh')
-        sleep 1
-      end
+
+    # ActiveSupport::Notifications.subscribe 'jobs.events' do |*args|
+    #   event = args.extract_options![:event] 
+    #   #   # p args.extract_options!
+    #   # #   p args.extract_options![:event]
+    #   # #   # sse.write({ time: Time.now }, event: 'Refresh') if args.extract_options![:event] == 'Refresh'
+    #   # #   # sleep 1
+    #   # #   # response.stream.write("messageType: '#{event}', data: #{data}\n\n")
+    #   # #   response.stream.write({  }, )
+    #   # #   response.stream.write("event: 'Refresh'\ndata: #{time: Time.now}\n\n")
+    # end
+
+    # loop do
+    #   sse.write({ time: Time.now }, event: 'Refresh') 
+    #   # response.stream.write "event: 'Refresh'\ndata: #{JSON.dump({ time: Time.now })}\n\n"
+    #   sleep 1
+    # end
+
+    User.on_change do
+      sse.write({ time: Time.now }, event: 'Refresh') 
+    end
+
     rescue IOError
+      logger.info "Stream closed"
       # When the client disconnects, we'll get an IOError on write
     ensure
+      logger.info "Events action is quitting and closing stream!"
       sse.close
-    end
+      # response.stream.close
   end
-  
 end
